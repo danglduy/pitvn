6068066a2a36e8c8a4133cb315d61715793e7cf3
danglduy <38782010+danglduy@users.noreply.github.com>
2023-07-09 13:48:51 +0700

    Fix eslint error when using `require`

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 export default {
   plugins: {
     tailwindcss: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
